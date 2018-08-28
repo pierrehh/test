33bea85fccc75e4041c670050f6d99c5348f1bd7
pierre <291417670@qq.com>
2018-08-28 23:44:58 +0800

    [test] this is test program

---
+++ webtest.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+
+print("Content-type:text/html")
+print()
+print('<html>')
+print('<head>')
+print('<title>Hello</title>')
+print('</head>')
+print('<body>')
+print('<h2>Hello Word! This is my first CGI program</h2>')
+print('</body>')
+print('</html>')
